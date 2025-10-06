73d6adaa25abff517f6596f099acb29577499d71
Paweł Kossakowski <pawel.kossakowski97@gmail.com>
2025-10-06 16:47:13 +0200

    Add function to add books with duplicate check

--- main.py
+++ main.py
@@ -25,6 +25,29 @@ def init_db():
     conn.close()
 
 
+def add_book(title, author, year):
+    """
+    Dodaje nową książkę do tabeli 'books'
+    """
+    conn = sqlite3.connect('library.db')
+    c = conn.cursor()
+    # Sprawdzenie, czy książka już istnieje
+    c.execute(
+        "SELECT * FROM books WHERE title = ? AND author = ? AND year = ?",
+        (title, author, year)
+    )
+    if not c.fetchone():
+        # Dodanie książki do bazy danych
+        c.execute(
+            "INSERT INTO books (title, author, year, available) VALUES (?, ?, ?, ?)",
+            (title, author, year, True)
+        )
+        conn.commit()
+    conn.close()
+
+
 if __name__ == "__main__":
     init_db()
-    print("Baza danych została zainicjalizowana!")
+    print("Baza danych została zainicjalizowana.")
+    add_book("It", "Stephen King", 1986)
+    print("Dodano książkę do bazy danych.")
